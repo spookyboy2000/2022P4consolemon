f4177c951777c3d0ac583768cdc5985b26bc47d2
Nick <32973@ma-web.nl>
2022-05-09 15:03:40 +0200

    third commit

---
+++ ConsoleMon/Consolemon/Consolemon/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Consolemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Consolemon dragon = new Consolemon();
+            Consolemon pidgeon = new Consolemon(100,"rat",1);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleMon/Consolemon/Consolemon/Skill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolemon
+{
+    internal class Skill
+    {
+        private string name;
+        private int damage;
+        private int energyCost;
+
+        internal Skill()
+        {
+            name = "firebreath";
+            damage = 50;
+            energyCost = 3;
+        }
+    }
+}
